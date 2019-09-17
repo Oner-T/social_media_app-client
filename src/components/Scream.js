8140275fad65d1fd800d6dd930d913a0eecdefee
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2019-09-17 13:24:25 +0300

    continue

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
+import {Link} from "react-router-dom";
 
+import withStyles from "@material-ui/core/styles/withStyles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -27,13 +28,27 @@ export class Scream extends Component {
         likeCount,
         commentCount
       }
-    } = this.props.return(
-      <Card>
-        <CardMedia 
-        image={userImage}
-        title = "Profile image" />
-        <CardContent>
-          <Typography></Typography>
+    } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {createdAt}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
     );
--- src/pages/home.js
+++ src/pages/home.js
@@ -19,7 +19,7 @@ class home extends Component {
   }
   render() {
       let recentScreamsMarkup = this.state.screams ? (
-          this.state.screams.map(scream => <Scream scream={scream}>)
+          this.state.screams.map(scream => <Scream scream={scream}/>)
       ) : <p>Loading...</p>
     return (
       <Grid container spacing={16}>
