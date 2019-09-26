220fe2ce68691f67bbe137e368e5d5f851697d85
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2019-09-27 00:36:48 +0300

    continue

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,13 +3,14 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
 import appIcon from "../images/icon.png";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 //MUI
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { CircularProgress } from "@material-ui/core";
 
 const styles = {
   form: {
@@ -25,12 +26,16 @@ const styles = {
     margin: "10px auto 10px auto"
   },
   button: {
-    marginTop: 20
+    marginTop: 20,
+    position: "relative",
   },
   customError: {
-    color:"red",
-    fontSize:"0.8rem",
-    marginTop:"10"
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: "10"
+  },
+  progress: {
+    position:"absolute"
   }
 };
 
@@ -122,9 +127,12 @@ class login extends Component {
               className={classes.button}
             >
               Login
+              {loading && <CircularProgress size={30} className={classes.progress} />}
             </Button>
             <br />
-            <small>don't have an account ? sign up <Link to="/signup"></Link></small>
+            <small>
+              don't have an account ? sign up <Link to="/signup"></Link>
+            </small>
           </form>
         </Grid>
         <Grid item sm />
