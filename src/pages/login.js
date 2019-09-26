c1216a4f27359efece35b741a3d0475b4d28e6ea
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2019-09-26 22:16:31 +0300

    continue

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,37 +2,130 @@ import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
 import appIcon from "../images/icon.png";
+import axios from "axios";
+import {Link} from "react-router-dom";
 
 //MUI
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const styles = {
   form: {
     textAlign: "center"
   },
   image: {
-      margin:"20px auto 20px auto"
+    margin: "20px auto 20px auto"
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto"
+  },
+  textField: {
+    margin: "10px auto 10px auto"
+  },
+  button: {
+    marginTop: 20
+  },
+  customError: {
+    color:"red",
+    fontSize:"0.8rem",
+    marginTop:"10"
   }
 };
 
-handleSubmit = () => {
-
-}
-
 class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {}
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post("/login", userData)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ loading: false });
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        });
+      });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
     const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
         <Grid item sm>
-          <img src={appIcon} alt="monkey" className={classes.image}/>
+          <img src={appIcon} alt="monkey" className={classes.image} />
           <Typography variant="h2" className={classes.pageTitle}>
-              Login
+            Login
           </Typography>
-          <form noValidate onSubmit={this.handleSubmit}></form>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.TextField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.TextField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Login
+            </Button>
+            <br />
+            <small>don't have an account ? sign up <Link to="/signup"></Link></small>
+          </form>
         </Grid>
         <Grid item sm />
       </Grid>
